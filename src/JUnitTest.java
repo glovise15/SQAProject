24fe5d4ba30d01770eb3bad84c892414ca321928
Gary <gary.lovisetto@student.uclouvain.be>
2017-04-14 10:28:35 +0200

    5 firt test cases

--- src/JUnitTest.java
+++ src/JUnitTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -7,26 +7,49 @@ import org.junit.Before;
 import org.junit.Test;
 
 import jodd.util.StringTemplateParser;
+import jodd.util.StringTemplateParser.MacroResolver;
 
 public class JUnitTest {
 	String template;
-	Map map;
+	Map<String, String> map;
 	StringTemplateParser stp;
+	MacroResolver mr;
 
 	@Before
 	public void setUp() throws Exception {
 		stp = new StringTemplateParser();
-		map = new HashMap();
+		map = new HashMap<String, String>();
+		map.put("foo", "Jodd");
+		mr = StringTemplateParser.createMapMacroResolver(map);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testTemplateNull() {
+		stp.parse(null, mr);
 	}
 
 	@Test
-	public void testTemplateNull() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
 	public void testTemplateEmpty() {
-		fail("Not yet implemented");
+		String result = stp.parse("", mr);
+		assertEquals(result, "");
+	}
+
+	@Test
+	public void testTemplateWithoutMacro() {
+		String result = stp.parse("Hello", mr);
+		assertEquals(result, "Hello");
+	}
+
+	@Test
+	public void testTemplateWithOneMacro() {
+		String result = stp.parse("Hello ${foo}", mr);
+		assertEquals(result, "Hello Jodd");
+	}
+
+	@Test
+	public void testTemplateWithIncorrectSyntax() {
+		String result = stp.parse("Hello ${foo}, %{foo}", mr);
+		assertEquals(result, "Hello Jodd, %{foo}");
 	}
 
 }
