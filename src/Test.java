f66eef5edcb100eabae02d739fd1b24163091a28
Gary <gary.lovisetto@student.uclouvain.be>
2017-04-14 10:05:10 +0200

    createMapMacroResolver

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 import jodd.util.StringTemplateParser;
+import jodd.util.StringTemplateParser.MacroResolver;
 
 public class Test {
-	StringTemplateParser stp = new StringTemplateParser();
+	public static void main(String[] args) {
+	    String template = "Hello ${foo}. Today is ${dayName}.";
+	    // prepare data
+	    Map<String, String> map = new HashMap<String, String>();
+	    map.put("foo", "Jodd");
+	    map.put("dayName", "Sunday");
+	    
+	    // parse
+	    StringTemplateParser stp = new StringTemplateParser();
+	    MacroResolver mr = stp.createMapMacroResolver(map);
+	    String result = stp.parse(template, mr);
+
+	    System.out.println(result);
+	}
 }
